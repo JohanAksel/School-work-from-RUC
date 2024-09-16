691b7ca0074742d2f3110c28d882d452afce7261
JohanAksel <146431479+JohanAksel@users.noreply.github.com>
2024-09-16 19:31:50 +0200

    Add files via upload
    Extremely simple program which shows what date it is in European and American format

---
+++ Date.java
@@ -0,0 +1,16 @@
+public class Date {
+        public static void main(String[] args) {
+        // make list of variables
+        String day = "monday";
+        String month = "september";
+        int date = 15;
+        int year = 2024;
+        System.out.println("American format: "+day+", "+month+" "+date+", "+year); //american format
+        System.out.println( "European format: "+day+" "+date+" "+month+" "+year); //Second assignment
+        
+        }
+    
+
+   
+
+}
